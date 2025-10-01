0d558872ba1e06d58eea7242db21c8e6ab0b8c23
rovertnet <matundukabamba@gmail.com>
2025-10-01 02:24:22 +0100

    another commit

---
+++ gdocs-frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue-900 py-10">
+        <h1 className="text-white text-3xl font-bold text-center">Welcome to GDocs</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
